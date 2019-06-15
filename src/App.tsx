eb47f4904b68c6edf9b372dcbfe3cd67e3183f6c
shozawa <tomohiro.shozawa@gmail.com>
2019-06-15 10:15:04 +0900

    add linter and prettier setting

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const names = ['Alice', 'Bob', 'Carol'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {names.map(name => (
+          <p>Hello, {name}.</p>
+        ))}
       </header>
     </div>
   );
-}
+};
 
 export default App;
